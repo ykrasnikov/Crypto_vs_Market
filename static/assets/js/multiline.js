50cbfe8fb522596c89d50e0ab30ca116ee7e8efe
Justin Merryman <justinmerryman@yahoo.com>
2021-03-31 16:29:33 -0500

    updated scaling

--- static/assets/js/multiline.js
+++ static/assets/js/multiline.js
@@ -1,4 +1,20 @@
+let height = plot3.offsetHeight;
+let width = plot3.offsetWidth;
+makeResponsive(width, height);
 
+// Event listener for window resize.
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", getSize);
+
+function getSize(){
+
+  console.log("window changed");
+  let height = plot3.offsetHeight;
+  let width = plot3.offsetWidth;
+  makeResponsive(width, height)
+}
+
+function makeResponsive(width, height){
   scrapeData('BTC', 1, "01-01-2013", "03-01-2021");
   scrapeData('LTC', 2, "01-01-2013", "03-01-2021");
   
@@ -80,16 +96,19 @@
     };
   }
 
-  let svgArea = d3.select("#plot3").select("svg");
-  
+  let svgArea = d3.select("body").select("#plot3").select("svg");
+  console.log(svgArea);  
   if (!svgArea.empty()) {
     svgArea.remove();
   }
 
   // SVG wrapper dimensions are determined by the current width
   // and height of the browser window.
-  let svgWidth = plot3.offsetWidth;
-  let svgHeight = plot3.offsetHeight;
+  // let svgWidth = plot3.offsetWidth;
+  // let svgHeight = plot3.offsetHeight;
+  let svgHeight = height;
+  let svgWidth = width;
+  
 
   let margin = {
     top: 30,
@@ -104,6 +123,7 @@
   // Select body, append SVG area to it, and set its dimensions
   let svg = d3.select("#plot3")
     .append("svg")
+    
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
@@ -238,7 +258,7 @@
       }
       // Configure a line function called drawLine which will plot the x and y coordinates using our scales
       function buildLine(yLinearScale, color, number, dataArray) {
-        
+
         let line = d3.line()
           .x(d => xTimeScale(d[1]))
           .y(d => yLinearScale(d[0]));
@@ -256,46 +276,47 @@
         //   .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + 45 + (number * 20)})`)
         //   .classed(`${color}-text text`, true)
         //   .text(`${choice} Cost at Close`);
-
-        let circlesGroup = chartGroup.selectAll("circle")
+    let circlesGroup = chartGroup.selectAll("circle")
           .data(dataArray)
           .enter()
           .append("circle")
           .attr("id", "label1")
           .attr("cx", d => xTimeScale(d[1]))
           .attr("cy", d => yLinearScale(d[0]))
-          .attr("r", "2")
-          .classed(`${color} label`, true);
+          .attr("r", "0.5")
+          .classed(`${color} label`, true)
+          .on("click", function(event, d) {
+            d3.select(".result")
+            .attr("font-size", "16px")
+            .text(`${choice} Cost: ${d[0]}`);
+            d3.select(".result2")
+            .attr("font-size", "16px")
+            .text(`Date: ${d[1]}`)
+            
+          })
+          
+        // // Step 1: Append a div to the body to create tooltips, assign it a class
+        // // =======================================================
+        // let toolTip = d3.select("#plot3").append("div")
+        //   .attr("class", "tooltip", true);
+        // // d3.select(".result").append("text")
+        // //     .attr("font-size", "16px")
+        // //     .text("is this working");
+        // // Step 2: Add an onmouseover event to display a tooltip
+        // // ========================================================
 
-        // Step 1: Append a div to the body to create tooltips, assign it a class
-        // =======================================================
-        let toolTip = d3.select("#chart").append("div")
-          .attr("class", "tooltip", true);
-
-        // Step 2: Add an onmouseover event to display a tooltip
-        // ========================================================
-
-        circlesGroup.on("mouseover", function (event, d) {
-        
-          toolTip.style("display", "block");
-          // let print = dataArray2.filter(word => word[1] == d[1])
-          console.log(d[0])
-          toolTip.html(`<strong> ${choice} Cost:${d[0]}, <br> Date: ${d[1]}</strong>`)
-            .style("left", (event.pageX) + "px")
-            .style("top", (event.pageY) + "px");
-          // toolTip.style("background", `${color}`
-        })
-          // Step 3: Add an onmouseout event to make the tooltip invisible
-          .on("mouseout", function () {
-            toolTip.style("display", "none");
-          });
+        // circlesGroup.on("mouseover", function (event, d) {
+        //   // let print = dataArray2.filter(word => word[1] == d[1])
+        //   toolTip.html(`<strong> ${choice} Cost:${d[0]}, <br> Date: ${d[1]}</strong>`)
+        //     .style("left", (event.pageX) + "px")
+        //     .style("top", (event.pageY) + "px");
+        //   // toolTip.style("background", `${color}`
+        // })
+        //   // Step 3: Add an onmouseout event to make the tooltip invisible
+        //   .on("mouseout", function () {
+        //     toolTip.style("display", "none");
+        //   });
       }
     }
   }
-
-// When the browser loads, makeResponsive() is called.
-// makeResponsive();
-
-// When the browser window is resized, responsify() is called.
-// d3.select("window").on("resize", makeResponsive);
-
+};
